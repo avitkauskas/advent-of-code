a00e3fbb13f9ba0e09c2a3a9782257de3b96a80c
Alvydas Vitkauskas <alvydas.vitkauskas@gmail.com>
2025-12-02 14:12:46 +0200

    day02 F#

--- 2025/fsharp/Days/Day02.fs
+++ 2025/fsharp/Days/Day02.fs
@@ -17,15 +17,16 @@ let isInvalidIdPart2 (n: int64) =
     let s = string n
     Regex.IsMatch(s, "^(.+?)\\1+$")
 
-let sumInvalidIds (isInvalid: int64 -> bool) input =
-    parseRanges input
+let sumInvalidIds (isInvalid: int64 -> bool) ranges =
+    ranges
     |> Seq.sumBy (fun (startId, endId) -> seq { startId..endId } |> Seq.filter isInvalid |> Seq.sum)
 
-let part1 input = sumInvalidIds isInvalidIdPart1 input
-let part2 input = sumInvalidIds isInvalidIdPart2 input
+let part1 ranges = sumInvalidIds isInvalidIdPart1 ranges
+let part2 ranges = sumInvalidIds isInvalidIdPart2 ranges
 
 let run () =
     let input = Input.readInputText ()
+    let ranges = parseRanges input
 
-    printfn "Day 02 - Part 1: %d" (part1 input)
-    printfn "Day 02 - Part 2: %d" (part2 input)
+    printfn "Day 02 - Part 1: %d" (part1 ranges)
+    printfn "Day 02 - Part 2: %d" (part2 ranges)
