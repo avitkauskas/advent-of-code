c85be1799e22627fd7d6129b295b3043abfca734
Alvydas Vitkauskas <alvydas.vitkauskas@gmail.com>
2025-12-11 11:03:44 +0200

    day11 F# initial

--- 2025/fsharp/Days/Day11.fs
+++ 2025/fsharp/Days/Day11.fs
@@ -3,62 +3,50 @@ module Day11
 open System.Collections.Generic
 open Utils
 
-let parse (lines: string[]) =
+let parse lines =
     lines
     |> Array.map (fun line ->
-        let parts = line.Split(": ")
-        let src = parts.[0]
-        let dsts = parts.[1].Split(' ') |> Array.toList
-        src, dsts)
+        let p = line.Split(": ")
+        p.[0], p.[1].Split(' ') |> Array.toList)
     |> dict
 
-/// Unified solver: either 0 required nodes, or exactly 2 required nodes.
-let countPaths (graph: IDictionary<string, list<string>>) (startNode: string) (required: string list) : int64 =
-
-    // We assume required is [] or [a;b]
-    let reqA, reqB =
-        match required with
+let countPaths (g: IDictionary<string, list<string>>) start req =
+    // req = [] or [a; b]
+    let a, b =
+        match req with
         | [] -> "", ""
-        | [ a; b ] -> a, b
-        | _ -> failwith "Incorrect use: required must be [] or [a; b]"
+        | [ x; y ] -> x, y
+        | _ -> failwith "expected 0 or 2 required nodes"
 
     let memo = Dictionary<(string * bool * bool), int64>()
 
-    let rec dfs node seenA seenB (stack: HashSet<string>) =
-        if stack.Contains(node) then
+    let rec dfs node sa sb (stack: HashSet<_>) =
+        if stack.Contains node then
             0L
         else
-            let seenA' = seenA || node = reqA
-            let seenB' = seenB || node = reqB
 
-            let key = (node, seenA', seenB')
+            let sa, sb = sa || node = a, sb || node = b
+            let key = node, sa, sb
 
-            match memo.TryGetValue(key) with
+            match memo.TryGetValue key with
             | true, v -> v
             | _ ->
-                let result =
+                let res =
                     if node = "out" then
-                        // If no required nodes ⇒ always count
-                        // If two required nodes ⇒ need both seen
-                        if required.IsEmpty || (seenA' && seenB') then 1L else 0L
+                        if req.IsEmpty || (sa && sb) then 1L else 0L
                     else
-                        stack.Add(node) |> ignore
-                        let sum = graph.[node] |> List.sumBy (fun child -> dfs child seenA' seenB' stack)
-                        stack.Remove(node) |> ignore
+                        stack.Add node |> ignore
+                        let sum = g.[node] |> List.sumBy (fun c -> dfs c sa sb stack)
+                        stack.Remove node |> ignore
                         sum
 
-                memo[key] <- result
-                result
+                memo.[key] <- res
+                res
 
-    dfs startNode false false (HashSet())
-
+    dfs start false false (HashSet())
 
 let run () =
-    let lines = Input.readInputLines ()
-    let graph = parse lines
+    let g = parse (Input.readInputLines ())
 
-    let part1 = countPaths graph "you" []
-    printfn "Day 11 - Part 1: %d" part1
-
-    let part2 = countPaths graph "svr" [ "dac"; "fft" ]
-    printfn "Day 11 - Part 2: %d" part2
+    printfn "Day 11 - Part 1: %d" (countPaths g "you" [])
+    printfn "Day 11 - Part 2: %d" (countPaths g "svr" [ "dac"; "fft" ])
