6b0077d771828e28447fff20bf3edf2f7978645b
Alvydas Vitkauskas <alvydas.vitkauskas@gmail.com>
2026-08-12 16:06:00 +0300

    d02 haskell

--- 2019/haskell/app/Day01.hs
+++ 2019/haskell/app/Day01.hs
@@ -1,4 +1,22 @@
-module Main (main) where
+module Main where
+
+fuel :: Int -> Int
+fuel mass = mass `div` 3 - 2
+
+totalFuel :: Int -> Int
+totalFuel mass = go mass 0
+  where
+    go m acc
+      | f <= 0 = acc
+      | otherwise = go f (acc + f)
+      where
+        f = fuel m
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  input <- readFile "../input/d01.txt"
+  let masses = map read $ lines input :: [Int]
+  let part1 = sum $ map fuel masses
+  let part2 = sum $ map totalFuel masses
+  putStrLn $ "part1: " ++ show part1
+  putStrLn $ "part2: " ++ show part2
---
+++ 2019/haskell/app/Day02.hs
@@ -0,0 +1,56 @@
+module Main (main) where
+
+import Control.Monad.ST (ST, runST)
+import Data.Array.ST (STUArray, newListArray, readArray, writeArray)
+
+targetOutput :: Int
+targetOutput = 19690720
+
+runIntcode :: [Int] -> Int
+runIntcode inputValues = runST $ do
+  mem <- newListArray (0, length inputValues - 1) inputValues :: ST s (STUArray s Int Int)
+  execute mem 0
+  where
+    execute :: STUArray s Int Int -> Int -> ST s Int
+    execute mem instrPtr = do
+      opcode <- readArray mem instrPtr
+      if opcode == 99
+        then readArray mem 0
+        else do
+          srcAddr1 <- readArray mem (instrPtr + 1)
+          srcAddr2 <- readArray mem (instrPtr + 2)
+          resultAddr <- readArray mem (instrPtr + 3)
+          operand1 <- readArray mem srcAddr1
+          operand2 <- readArray mem srcAddr2
+          writeArray mem resultAddr (apply opcode operand1 operand2)
+          execute mem (instrPtr + 4)
+
+    apply :: Int -> Int -> Int -> Int
+    apply 1 a b = a + b
+    apply 2 a b = a * b
+    apply opcode _ _ = error $ "unknown opcode " ++ show opcode
+
+withNounVerb :: Int -> Int -> [Int] -> [Int]
+withNounVerb noun verb (x0 : _ : _ : rest) = x0 : noun : verb : rest
+withNounVerb _ _ _ = error "input too short"
+
+findNounVerb :: [Int] -> (Int, Int)
+findNounVerb inputValues =
+  case
+    [ (noun, verb)
+      | noun <- [0 .. 99],
+        verb <- [0 .. 99],
+        runIntcode (withNounVerb noun verb inputValues) == targetOutput
+    ] of
+    (noun, verb) : _ -> (noun, verb)
+    [] -> error "no noun/verb found"
+
+main :: IO ()
+main = do
+  input <- readFile "../input/d02.txt"
+  let inputValues = map read (words (map (\c -> if c == ',' then ' ' else c) input)) :: [Int]
+      part1 = runIntcode (withNounVerb 12 2 inputValues)
+      (noun, verb) = findNounVerb inputValues
+      part2 = 100 * noun + verb
+  putStrLn $ "Part 1: Value at position 0: " ++ show part1
+  putStrLn $ "Part 2: 100 * noun + verb = " ++ show part2
